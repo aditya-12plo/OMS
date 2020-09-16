4ccb23f2800460ad214bed4e2a3901b73492fc33
aditya-8commerce <nugroho.aditya@8commerce.com>
2020-09-16 12:22:19 +0700

    show error to json

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -49,6 +49,15 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        // return parent::render($request, $exception);
+
+        $rendered = parent::render($request, $exception);
+
+        return response()
+        ->json(['status'=>$rendered->getStatusCode() ,'datas' => [], 'errors' => ['code' => $rendered->getStatusCode() , 'message' => $exception->getMessage()]])
+        ->withHeaders([
+            'Content-Type'          => 'application/json',
+            ])
+        ->setStatusCode($rendered->getStatusCode());
     }
 }
